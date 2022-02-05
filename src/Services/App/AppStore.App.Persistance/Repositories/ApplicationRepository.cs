b3410e0feb34d63c1668dd24bca36a9039c628dd
Lucas Rafaell <lucas.rafaell@outloo.com>
2022-02-05 20:34:25 -0300

    feat(persistence) - Create persistence project in app microservice

---
+++ src/Services/App/AppStore.App.Persistance/Repositories/ApplicationRepository.cs
@@ -0,0 +1,29 @@
+﻿using AppStore.App.Domain.Repositories;
+using AppStore.Common.Domain.Entities;
+using AppStore.Common.Domain.Persistence.MongoDB;
+using AppStore.Common.Infrastructure.Persistance.Base;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace AppStore.App.Persistance.Repositories
+{
+    public sealed class ApplicationRepository : Repository<Application>, IApplicationRepository
+    {
+        #region Constructor
+
+        public ApplicationRepository(IMongoContext context) : base(context) { }
+
+        #endregion
+
+        #region IApplicationRepository Methods
+
+        public async Task<Application> GetApplicationByNameAsync(string name)
+        {
+            var result = await collection.FindAsync(Builders<Application>.Filter.Eq("Name", name));
+
+            return result.FirstOrDefault();
+        }
+
+        #endregion
+    }
+}
